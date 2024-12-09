9f0bd3cd3b62b7640e62ba2d875bb2d3712fb654
X3M4 <jofermo5@gmail.com>
2024-12-09 12:47:59 +0000

    09-12-2024 - Mejoras en los botone, filtros y búsquedas

--- crm_lead_rejected_stage/models/crm_lead_custom_state.py
+++ crm_lead_rejected_stage/models/crm_lead_custom_state.py
@@ -16,56 +16,83 @@ class CrmLeadCustom(models.Model):
         # pdb.set_trace()
         self.action_check_all_registers()
         self.is_rejected = True
-        # return self.write({'probability': 0.1})
-        self.write({'probability': 0.05, 'active': True})
-        return {
-            'name': 'Rejected Reason',
-            'type': 'ir.actions.act_window',
-            'res_model': 'crm.lead.rejected',
-            'view_mode': 'form',
-            'view_id': self.env.ref('crm_lead_rejected_stage.crm_lead_rejected_view_form').id,
-            'target': 'new',
-            'context': {
-                'default_lead_id': self.id,  # Pasar el ID del lead al wizard
-            },
-        }
+        self.is_lost = False
+        self.is_won = False
+
+        self.write({'probability': 0.05, 
+                    'active': True, 
+                    'lost_reason': False, 
+                    'is_rejected': True, 
+                    'is_lost': False,
+                    'is_won': False})
+        # return {
+        #     'name': 'Rejected Reason',
+        #     'type': 'ir.actions.act_window',
+        #     'res_model': 'crm.lead.rejected',
+        #     'view_mode': 'form',
+        #     'view_id': self.env.ref('crm_lead_rejected_stage.crm_lead_rejected_view_form').id,
+        #     'target': 'new',
+        #     'context': {
+        #         'default_lead_id': self.id,  # Pasar el ID del lead al wizard
+        #     },
+        # }
     
     @api.multi
     def action_set_lost(self):
         """ Lost semantic: probability = 0, active = False """
         self.action_check_all_registers()
         self.is_lost = True
-        return self.write({'probability': 0, 'active': False})
+        self.is_rejected = False
+        self.is_won = False
+        self.rejected_reason_id = False
+        return self.write({'probability': 0,
+                           'active': False, 
+                           'rejected_reason_id': False,
+                           'is_lost': True,
+                           'is_rejected': False,
+                           'is_won': False})
     
     @api.multi
     def action_set_won(self):
         """ Won semantic: probability = 100 (active untouched) """
         self.action_check_all_registers()
         for lead in self:
+            self.is_lost = False
+            self.is_rejected = False
             self.is_won = True
             # stage_id = lead._stage_find(domain=[('probability', '=', 100.0), ('on_change', '=', True)])
             # lead.write({'stage_id': stage_id.id, 'probability': 100})
-            return self.write({'probability': 100, 'active': True})
+            return self.write({'probability': 100, 
+                               'active': True, 
+                               'rejected_reason_id': False,
+                               'lost_reason': False,
+                               'is_won': True,
+                               'is_lost': False,
+                               'is_rejected': False})
     
     @api.multi
     def action_check_all_registers(self):
-        leads = self.env['crm.lead'].search([])
+        leads = self.env['crm.lead']
         for l in leads:
             if l.probability == 0 and l.active == False:
                 l.write({
                     'is_lost': True,
                     'is_rejected': False,
-                    'is_won': False})
+                    'is_won': False,
+                    'rejected_reason_id': False},)
             elif l.probability == 100:
                 l.write({
-                    'is_won': True,
+                    'is_won':True,
                     'is_lost': False,
-                    'is_rejected': False})
+                    'is_rejected': False,
+                    'rejected_reason_id': False,
+                    'lost_reason' : False},)
             else:
                 l.write({
                     'is_rejected': True,
                     'is_lost': False,
-                    'is_won': False})
+                    'is_won': False,
+                    'lost_reason': False})
     
 class RejectedReason(models.Model):
     _name = 'crm.rejected.reason'
