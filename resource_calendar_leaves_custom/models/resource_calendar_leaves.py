e427ff694f6e002dab73537cbcd74195d7d28c3e
X3M4 <jofermo5@gmail.com>
2024-12-20 11:19:50 +0000

    Solucionado recálculo de fetivos nacionales incluído Jueves Santo y Viernes Santo

--- resource_calendar_leaves_custom/models/resource_calendar_leaves.py
+++ resource_calendar_leaves_custom/models/resource_calendar_leaves.py
@@ -3,58 +3,93 @@ from datetime import datetime
 
 class ResourceCalendarCustom(models.Model):
     _inherit = 'resource.calendar'
+    
+    def calculate_easter_date(self, year):
+        """Calculate the date of Easter Sunday using the Gauss algorithm."""
+        a = year % 19
+        b = year // 100
+        c = year % 100
+        d = b // 4
+        e = b % 4
+        f = (b + 8) // 25
+        g = (b - f + 1) // 3
+        h = (19 * a + b - d - g + 15) % 30
+        i = c // 4
+        k = c % 4
+        l = (32 + 2 * e + 2 * i - h - k) % 7
+        m = (a + 11 * h + 22 * l) // 451
+        month = (h + l - 7 * m + 114) // 31  # March = 3, April = 4
+        day = ((h + l - 7 * m + 114) % 31) + 1
+        return datetime(year, month, day, 0, 0, 0)
+
+    def calculate_holy_thursday_good_friday(self, year):
+        """Calculate the dates of Holy Thursday and Good Friday for a given year."""
+        from datetime import date, timedelta
+
+        # Calculate Easter Sunday
+        
+        easter_sunday = self.calculate_easter_date(year)
+
+        # Holy Thursday is 3 days before Easter Sunday
+        holy_thursday = easter_sunday - timedelta(days=3)
+        # Good Friday is 2 days before Easter Sunday
+        good_friday = easter_sunday - timedelta(days=2)
+
+        return holy_thursday, good_friday
 
     @api.model
     def _get_default_global_leaves(self):
         current_year = datetime.now().year
+        thursday,friday = self.calculate_holy_thursday_good_friday(current_year)
         return [
-            (0, 0, {'name': 'Año Nuevo', 'date_from': f'{current_year}-01-01 00:00:00', 'date_to': f'{current_year}-01-02 00:00:00'}),
-            (0, 0, {'name': 'Reyes', 'date_from': f'{current_year}-01-06 00:00:00', 'date_to': f'{current_year}-01-07 00:00:00'}),
-            (0, 0, {'name': 'Día de la Constitución', 'date_from': f'{current_year}-12-06 00:00:00', 'date_to': f'{current_year}-12-07 00:00:00'}),
-            (0, 0, {'name': 'Viernes Santo', 'date_from': f'{current_year}-04-18 00:00:00', 'date_to': f'{current_year}-04-19 00:00:00'}),
-            (0, 0, {'name': 'Día del Trabajador', 'date_from': f'{current_year}-05-01 00:00:00', 'date_to': f'{current_year}-05-02 00:00:00'}),
-            (0, 0, {'name': 'Asunción de la Virgen', 'date_from': f'{current_year}-08-15 00:00:00', 'date_to': f'{current_year}-08-16 00:00:00'}),
-            (0, 0, {'name': 'Fiesta Nacional de España', 'date_from': f'{current_year}-10-12 00:00:00', 'date_to': f'{current_year}-10-13 00:00:00'}),
-            (0, 0, {'name': 'Todos los Santos', 'date_from': f'{current_year}-11-01 00:00:00', 'date_to': f'{current_year}-11-02 00:00:00'}),
-            (0, 0, {'name': 'Inmaculada Concepción', 'date_from': f'{current_year}-12-08 00:00:00', 'date_to': f'{current_year}-12-09 00:00:00'}),
-            (0, 0, {'name': 'Navidad', 'date_from': f'{current_year}-12-25 00:00:00', 'date_to': f'{current_year}-12-26 00:00:00'}),
+            (0, 0, {'name': 'Año Nuevo', 'date_from': fields.Datetime.from_string(f'{current_year}-01-01'), 'date_to': fields.Datetime.from_string(f'{current_year}-01-02')}),
+            (0, 0, {'name': 'Reyes', 'date_from': fields.Datetime.from_string(f'{current_year}-01-06 00:00:00'), 'date_to': fields.Datetime.from_string(f'{current_year}-01-07 00:00:00')}),
+            (0, 0, {'name': 'Día de la Constitución', 'date_from': fields.Datetime.from_string(f'{current_year}-12-06 00:00:00'), 'date_to': fields.Datetime.from_string(f'{current_year}-12-07 00:00:00')}),
+            (0, 0, {'name': 'Jueves Santo', 'date_from': thursday, 'date_to': thursday}),
+            (0, 0, {'name': 'Viernes Santo', 'date_from': friday, 'date_to': friday}),
+            (0, 0, {'name': 'Día del Trabajador', 'date_from': fields.Datetime.from_string(f'{current_year}-05-01 00:00:00'), 'date_to': fields.Datetime.from_string(f'{current_year}-05-02 00:00:00')}),
+            (0, 0, {'name': 'Asunción de la Virgen', 'date_from': fields.Datetime.from_string(f'{current_year}-08-15 00:00:00'), 'date_to': fields.Datetime.from_string(f'{current_year}-08-16 00:00:00')}),
+            (0, 0, {'name': 'Fiesta Nacional de España', 'date_from': fields.Datetime.from_string(f'{current_year}-10-12 00:00:00'), 'date_to': fields.Datetime.from_string(f'{current_year}-10-13 00:00:00')}),
+            (0, 0, {'name': 'Todos los Santos', 'date_from': fields.Datetime.from_string(f'{current_year}-11-01 00:00:00'), 'date_to': fields.Datetime.from_string(f'{current_year}-11-02 00:00:00')}),
+            (0, 0, {'name': 'Inmaculada Concepción', 'date_from': fields.Datetime.from_string(f'{current_year}-12-08 00:00:00'), 'date_to': fields.Datetime.from_string(f'{current_year}-12-09 00:00:00')}),
+            (0, 0, {'name': 'Navidad', 'date_from': fields.Datetime.from_string(f'{current_year}-12-25 00:00:00'), 'date_to': fields.Datetime.from_string(f'{current_year}-12-26 00:00:00')}),
         ]
 
-    @api.model
-    def update_global_leaves(self):
-        current_year = datetime.now().year
-        leaves = self.env['resource.calendar.leaves'].search([('resource_id', '=', False)])
-        for leave in leaves:
-            if leave.name == 'Año Nuevo':
-                leave.date_from = f'{current_year}-01-01 00:00:00'
-                leave.date_to = f'{current_year}-01-02 00:00:00'
-            elif leave.name == 'Reyes':
-                leave.date_from = f'{current_year}-01-06 00:00:00'
-                leave.date_to = f'{current_year}-01-07 00:00:00'
-            elif leave.name == 'Día de la Constitución':
-                leave.date_from = f'{current_year}-12-06 00:00:00'
-                leave.date_to = f'{current_year}-12-07 00:00:00'
-            elif leave.name == 'Viernes Santo':
-                leave.date_from = f'{current_year}-04-18 00:00:00'
-                leave.date_to = f'{current_year}-04-19 00:00:00'
-            elif leave.name == 'Día del Trabajador':
-                leave.date_from = f'{current_year}-05-01 00:00:00'
-                leave.date_to = f'{current_year}-05-02 00:00:00'
-            elif leave.name == 'Asunción de la Virgen':
-                leave.date_from = f'{current_year}-08-15 00:00:00'
-                leave.date_to = f'{current_year}-08-16 00:00:00'
-            elif leave.name == 'Fiesta Nacional de España':
-                leave.date_from = f'{current_year}-10-12 00:00:00'
-                leave.date_to = f'{current_year}-10-13 00:00:00'
-            elif leave.name == 'Todos los Santos':
-                leave.date_from = f'{current_year}-11-01 00:00:00'
-                leave.date_to = f'{current_year}-11-02 00:00:00'
-            elif leave.name == 'Inmaculada Concepción':
-                leave.date_from = f'{current_year}-12-08 00:00:00'
-                leave.date_to = f'{current_year}-12-09 00:00:00'
-            elif leave.name == 'Navidad':
-                leave.date_from = f'{current_year}-12-25 00:00:00'
-                leave.date_to = f'{current_year}-12-26 00:00:00'
+    # @api.model
+    # def update_global_leaves(self):
+    #     current_year = datetime.now().year
+    #     leaves = self.env['resource.calendar.leaves'].search([('resource_id', '=', False)])
+    #     for leave in leaves:
+    #         if leave.name == 'Año Nuevo':
+    #             leave.date_from = fields.Datetime.from_string(f'{current_year}-01-01 00:00:00')
+    #             leave.date_to = datetime(current_year, 1, 2, 0, 0, 0)
+    #         elif leave.name == 'Reyes':
+    #             leave.date_from = datetime(current_year, 1, 6, 0, 0, 0)
+    #             leave.date_to = datetime(current_year, 1, 7, 0, 0, 0)
+    #         elif leave.name == 'Día de la Constitución':
+    #             leave.date_from = datetime(current_year, 12, 6, 0, 0, 0)
+    #             leave.date_to = datetime(current_year, 12, 7, 0, 0, 0)
+    #         elif leave.name == 'Viernes Santo':
+    #             leave.date_from = datetime(current_year, 4, 18, 0, 0, 0)
+    #             leave.date_to = datetime(current_year, 4, 19, 0, 0, 0)
+    #         elif leave.name == 'Día del Trabajador':
+    #             leave.date_from = datetime(current_year, 5, 1, 0, 0, 0)
+    #             leave.date_to = datetime(current_year, 5, 2, 0, 0, 0)
+    #         elif leave.name == 'Asunción de la Virgen':
+    #             leave.date_from = datetime(current_year, 8, 15, 0, 0, 0)
+    #             leave.date_to = datetime(current_year, 8, 16, 0, 0, 0)
+    #         elif leave.name == 'Fiesta Nacional de España':
+    #             leave.date_from = datetime(current_year, 10, 12, 0, 0, 0)
+    #             leave.date_to = datetime(current_year, 10, 13, 0, 0, 0)
+    #         elif leave.name == 'Todos los Santos':
+    #             leave.date_from = datetime(current_year, 11, 1, 0, 0, 0)
+    #             leave.date_to = datetime(current_year, 11, 2, 0, 0, 0)
+    #         elif leave.name == 'Inmaculada Concepción':
+    #             leave.date_from = datetime(current_year, 12, 8, 0, 0, 0)
+    #             leave.date_to = datetime(current_year, 12, 9, 0, 0, 0)
+    #         elif leave.name == 'Navidad':
+    #             leave.date_from = datetime(current_year, 12, 25, 0, 0, 0)
+    #             leave.date_to = datetime(current_year, 12, 26, 0, 0, 0)
 
     global_leave_ids = fields.One2many(
         'resource.calendar.leaves', 'calendar_id', 'Global Leaves',
--- resource_calendar_leaves_custom/wizard/update_global_leave_ids.py
+++ resource_calendar_leaves_custom/wizard/update_global_leave_ids.py
@@ -1,56 +1,111 @@
 from odoo import models, fields, api, _
 from datetime import datetime
+import sys
+from datetime import timedelta
 
 class UpdateGlobalLeaveIds(models.TransientModel):
     _name = 'update.global.leave.ids'
     _description = 'Update Global Leave Ids'
     
-    year = fields.Integer('Year', required=True, default=lambda self: fields.datetime.now().year + 1)
-    preview = fields.Html('Preview', compute='_compute_preview')
+    year = fields.Integer('Year', required=True, default=lambda self: fields.datetime.now().year)
     
-    def _compute_preview(self):
-        self.ensure_one()
-        leaves = self.env['resource.calendar.leaves'].search([('resource_id', '=', False)])
-        preview = '<ul>'
-        for leave in leaves:
-            preview += f'<li>{leave.name}: {leave.date_from} - {leave.date_to}</li><br>'
-        preview += '</ul>'
-        self.preview = preview
+    def calculate_easter_date(self, year):
+        """Calculate the date of Easter Sunday using the Gauss algorithm."""
+        a = year % 19
+        b = year // 100
+        c = year % 100
+        d = b // 4
+        e = b % 4
+        f = (b + 8) // 25
+        g = (b - f + 1) // 3
+        h = (19 * a + b - d - g + 15) % 30
+        i = c // 4
+        k = c % 4
+        l = (32 + 2 * e + 2 * i - h - k) % 7
+        m = (a + 11 * h + 22 * l) // 451
+        month = (h + l - 7 * m + 114) // 31  # March = 3, April = 4
+        day = ((h + l - 7 * m + 114) % 31) + 1
+        return datetime(year, month, day, 0, 0, 0)
 
-    def update_global_leave_ids(self):
+    def calculate_holy_thursday_good_friday(self, year):
+        """Calculate the dates of Holy Thursday and Good Friday for a given year."""
+        from datetime import date, timedelta
+
+        # Calculate Easter Sunday
+        
+        easter_sunday = self.calculate_easter_date(year)
+
+        # Holy Thursday is 3 days before Easter Sunday
+        holy_thursday = easter_sunday - timedelta(days=3)
+        # Good Friday is 2 days before Easter Sunday
+        good_friday = easter_sunday - timedelta(days=2)
+
+        return holy_thursday, good_friday
+
+    def wizard_update_global_leave_ids(self):
         self.ensure_one()
         current_year = self.year
         leaves = self.env['resource.calendar.leaves'].search([('resource_id', '=', False)])
-        calendars = self.env['resource.calendar'].search(['global_leave_ids', '!=', False])
+        calendars = self.env['resource.calendar'].search([('global_leave_ids', '!=', False)])
+        
+        # import pdb; pdb.set_trace()
     
         for leave in leaves:
             if leave.name == 'Año Nuevo':
-                leave.date_from = f'{current_year}-01-01 00:00:00'
-                leave.date_to = f'{current_year}-01-02 00:00:00'
+                leave.write({
+                    'date_from': datetime(self.year, 1, 1, 0, 0, 0),
+                    'date_to': datetime(self.year, 1, 1, 23, 59, 59)
+                })
             elif leave.name == 'Reyes':
-                leave.date_from = f'{current_year}-01-06 00:00:00'
-                leave.date_to = f'{current_year}-01-07 00:00:00'
+                leave.write({
+                    'date_from': datetime(self.year, 1, 6, 0, 0, 0),
+                    'date_to': datetime(self.year, 1, 6, 23, 59, 59)
+                })
             elif leave.name == 'Día de la Constitución':
-                leave.date_from = f'{current_year}-12-06 00:00:00'
-                leave.date_to = f'{current_year}-12-07 00:00:00'
+                leave.write({
+                    'date_from': datetime(self.year, 12, 6, 0, 0, 0),
+                    'date_to': datetime(self.year, 12, 6, 23, 59, 59)
+                })
+            elif leave.name == 'Jueves Santo':
+                thursday, friday = self.calculate_holy_thursday_good_friday(self.year)
+                leave.write({
+                    'date_from': thursday,
+                    'date_to': thursday + timedelta(days=1) - timedelta(hours=1)
+                })
             elif leave.name == 'Viernes Santo':
-                leave.date_from = f'{current_year}-04-18 00:00:00'
-                leave.date_to = f'{current_year}-04-19 00:00:00'
+                thursday, friday = self.calculate_holy_thursday_good_friday(self.year)
+                leave.write({
+                    'date_from': friday,
+                    'date_to': friday + timedelta(days=1) - timedelta(hours=1)
+                })
             elif leave.name == 'Día del Trabajador':
-                leave.date_from = f'{current_year}-05-01 00:00:00'
-                leave.date_to = f'{current_year}-05-02 00:00:00'
+                leave.write({
+                    'date_from': datetime(self.year, 5, 1, 0, 0, 0),
+                    'date_to': datetime(self.year, 5, 1, 23, 59, 59)
+                })
             elif leave.name == 'Asunción de la Virgen':
-                leave.date_from = f'{current_year}-08-15 00:00:00'
-                leave.date_to = f'{current_year}-08-16 00:00:00'
+                leave.write({
+                    'date_from': datetime(self.year, 8, 15, 0, 0, 0),
+                    'date_to': datetime(self.year, 8, 15, 23, 59, 59)
+                })
             elif leave.name == 'Fiesta Nacional de España':
-                leave.date_from = f'{current_year}-10-12 00:00:00'
-                leave.date_to = f'{current_year}-10-13 00:00:00'
+                leave.write({
+                    'date_from': datetime(self.year, 10, 12, 0, 0, 0),
+                    'date_to': datetime(self.year, 10, 12, 23, 59, 59)
+                })
             elif leave.name == 'Todos los Santos':
-                leave.date_from = f'{current_year}-11-01 00:00:00'
-                leave.date_to = f'{current_year}-11-02 00:00:00'
+                leave.write({
+                    'date_from': datetime(self.year, 11, 1, 0, 0, 0),
+                    'date_to': datetime(self.year, 11, 1, 23, 59, 59)
+                })
             elif leave.name == 'Inmaculada Concepción':
-                leave.date_from = f'{current_year}-12-08 00:00:00'
-                leave.date_to = f'{current_year}-12-09 00:00:00'
+                leave.write({
+                    'date_from': datetime(self.year, 12, 8, 0, 0, 0),
+                    'date_to': datetime(self.year, 12, 8, 23, 59, 59)
+                })
             elif leave.name == 'Navidad':
-                leave.date_from = f'{current_year}-12-25 00:00:00'
-                leave.date_to = f'{current_year}-12-26 00:00:00'
+                leave.write({
+                    'date_from': datetime(self.year, 12, 25, 0, 0, 0),
+                    'date_to': datetime(self.year, 12, 25, 23, 59, 59)
+                })
+            print(f"Actualizado: {leave.name}, {leave.date_from} - {leave.date_to}".encode(sys.stdout.encoding, errors='replace').decode())
